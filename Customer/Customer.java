a61842d34d9b25920be6cb45ea8871d50cc4b374
Tyler Hipolito <116042724+typoEXE@users.noreply.github.com>
2023-05-15 19:43:16 -0700

    Add files via upload

--- Customer/Customer.java
+++ Customer/Customer.java
@@ -1,1 +1,128 @@
+package customer;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="customer")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="street")
+	private String street;
+	
+	@Column(name="state")
+	private String state;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="zipcode")
+	private int zipcode;
+
+	public Customer() {
+		// No args
+	}
+	
+	public Customer(String name, String phone, String email, String street, String city, String state,
+			int zipcode) {
+		
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.street = street;
+		this.state = state;
+		this.city = city;
+		this.zipcode = zipcode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", street=" + street
+				+ ", state=" + state + ", city=" + city + ", zipcode=" + zipcode + "]";
+	}
+	
+	
+	
+}
